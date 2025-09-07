0e45925ab4be8f049fb5c0b3947632355814a766
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-09-07 14:51:15 +0200

    feat: localization lamp card

--- pages/[locale]/tinder-slides.tsx
+++ pages/[locale]/tinder-slides.tsx
@@ -1,11 +1,18 @@
+import { useTranslation } from 'react-i18next';
 import TinderCards from '../../components/TinderCards';
+import { getStaticPaths, makeStaticProps } from '@/lib/getStatic';
 
 export const TinderSlides = () => {
+  const { t } = useTranslation(['common']);
+
   return (
     <div>
-      <TinderCards />
+      <TinderCards t={t} />
     </div>
   );
 };
 
 export default TinderSlides;
+
+const getStaticProps = makeStaticProps(['second-page', 'common', 'footer']);
+export { getStaticPaths, getStaticProps };
