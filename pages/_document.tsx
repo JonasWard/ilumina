0e27d440698be02b550155e1965d20435c61ca0b
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-09-07 16:31:26 +0200

    feat: added icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,11 +13,7 @@ class MyDocument extends Document {
 
           <link href="https://cdnjs.cloudflare.com/ajax/libs/typicons/2.0.9/typicons.min.css" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400|Oswald:600" rel="stylesheet" />
-          <link
-            data-react-helmet="true"
-            rel="icon"
-            href="https://blobscdn.gitbook.com/v0/b/gitbook-28427.appspot.com/o/spaces%2F-L9iS6Wm2hynS5H9Gj7j%2Favatar.png?generation=1523462254548780&amp;alt=media"
-          />
+          <link data-react-helmet="true" rel="icon" href="/assets/icons/icon.svg" />
         </Head>
         <body>
           <Main />
