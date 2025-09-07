e060ae72a7fbe86da23e536ed11c51667849f493
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-09-07 12:13:29 +0200

    fix: tailwind + moved style sheets around

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { appWithTranslation } from 'next-i18next';
+import './globals.css';
 
 const MyApp = ({ Component, pageProps }: { Component: any; pageProps: any }) => <Component {...pageProps} />;
 
