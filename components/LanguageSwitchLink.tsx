9bcc313c3692ffd093aa1079d391198a485579fa
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-09-08 21:20:39 +0200

    feat: language switch as select

--- components/LanguageSwitchLink.tsx
+++ components/LanguageSwitchLink.tsx
@@ -1,32 +1,26 @@
-import languageDetector from '../lib/languageDetector';
+import { BASE_HREF } from '@/lib/config';
 import { useRouter } from 'next/router';
-import Link from 'next/link';
+import i18nextConfig from '../next-i18next.config';
 
-const LanguageSwitchLink: React.FC<{ locale: string; href: string }> = ({ locale, ...rest }) => {
+const LanguageSwitchLink: React.FC = () => {
   const router = useRouter();
-
-  let href = rest.href || router.asPath;
-  let pName = router.pathname;
-  Object.keys(router.query).forEach((k) => {
-    if (k === 'locale') {
-      pName = pName.replace(`[${k}]`, locale);
-      return;
-    }
-    pName = pName.replace(`[${k}]`, router.query[k] as string);
-  });
-  if (locale) {
-    href = rest.href ? `/${locale}${rest.href}` : pName;
-  }
-  if (href.indexOf(`/${locale}`) < 0) {
-    href = `/${locale}${href}`;
-  }
+  const currentLocale = router.query.locale || i18nextConfig.i18n.defaultLocale;
 
   return (
-    <Link href={href}>
-      <button style={{ fontSize: 'small' }} onClick={() => languageDetector.cache!(locale)}>
-        {locale}
-      </button>
-    </Link>
+    <select
+      value={currentLocale}
+      onChange={(e) => {
+        const locale = e.target.value;
+        const currentRelevantURL = router.asPath.replace(BASE_HREF !== '/' ? BASE_HREF + '/' : '/', '').split('/')[1];
+        router.push((BASE_HREF === '/' ? BASE_HREF : BASE_HREF + '/') + locale + '/' + currentRelevantURL);
+      }}
+    >
+      {i18nextConfig.i18n.locales.map((locale) => (
+        <option style={{ fontSize: 'small' }} key={locale} value={locale}>
+          {locale}
+        </option>
+      ))}
+    </select>
   );
 };
 
