baf5e013f8c46e7161b0da83db0c6b182d76b1d1
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-09-08 21:22:54 +0200

    chore: styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,13 +2,13 @@ import Head from 'next/head';
 import icon from '/assets/icons/icon.svg';
 import logo from '/assets/icons/logo.svg';
 
-export const Header: React.FC<{ heading: string; title: string }> = ({ heading, title }) => (
+export const Header: React.FC<{ heading: string; title: string; className?: string }> = ({ heading, title, className }) => (
   <>
     <Head>
       <link rel="icon" href={icon.src} />
       <title>{title}</title>
     </Head>
-    <img src={logo.src} alt="logo" className="max-w-[min(70vw,400px)] my-10" style={{ filter: 'invert(1)' }} />
-    <h1 className="text-slate-200">{heading}</h1>
+    <img src={logo.src} alt="logo" className="max-w-[min(70vw,400px)] my-10" />
+    <h1 className={className}>{heading}</h1>
   </>
 );
