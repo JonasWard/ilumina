36c9b868fbe96b01c80aaf4fbbf9b23b12a23546
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-09-08 21:21:20 +0200

    feat: working navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,16 +1,25 @@
 import logo from '/assets/icons/logo.svg';
+import { useTranslation } from 'react-i18next';
+import LanguageSwitchLink from './LanguageSwitchLink';
+import { BASE_HREF } from '@/lib/config';
 
-export const Navigation: React.FC<{ heading: string }> = ({ heading }) => (
-  <div className="fixed top-0 left-0 w-full my-2">
-    <div className="w-[min(80vw,400px)] mx-auto flex flex-row items-center justify-between">
-      <img
-        src={logo.src}
-        alt="logo"
-        className="max-h-[max(min(5svh,5svw),20px)]"
-        style={{ filter: 'invert(1)' }}
-        onClick={() => (document.location.href = '/')}
-      />
-      <span className="text-slate-200 text-[max(min(5svh,5svw),20px)]">{heading}</span>
+export const Navigation: React.FC<{ heading: string }> = ({ heading }) => {
+  const { t } = useTranslation(['navigation']);
+
+  return (
+    <div className="fixed top-0 left-0 w-[100svw] my-2">
+      <div className="w-[min(80vw,1200px)] mx-auto flex flex-row items-center justify-between">
+        <img
+          src={logo.src}
+          alt="logo"
+          className="max-h-[max(min(5svh,5svw),20px)]"
+          onClick={() => (document.location.href = BASE_HREF)}
+        />
+        <span className="text-sm">
+          {t('change-locale')}:
+          <LanguageSwitchLink />
+        </span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
