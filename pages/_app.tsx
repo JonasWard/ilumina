d5dd51099d6a7bcf6901786c95c56917d06bca10
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-11-17 14:59:54 +0100

    feat: storing liked / disliked in localStorage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import { appWithTranslation } from 'next-i18next';
 import './globals.css';
+import { LOCAL_STORAGE_KEY_DISLIKED, LOCAL_STORAGE_KEY_LIKED, useLampStore } from '@/lib/store';
+import { useEffect } from 'react';
 
-const MyApp = ({ Component, pageProps }: { Component: any; pageProps: any }) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }: { Component: any; pageProps: any }) => {
+  useEffect(() => {
+    if (localStorage.getItem(LOCAL_STORAGE_KEY_LIKED))
+      useLampStore.setState(() => ({
+        liked: new Set(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_LIKED)!))
+      }));
+    
+    if (localStorage.getItem(LOCAL_STORAGE_KEY_DISLIKED))
+      useLampStore.setState(() => ({
+        disliked: new Set(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_DISLIKED)!))
+      }));
+  }, []);
+
+  return <Component {...pageProps} />;
+};
 
 export default appWithTranslation(MyApp);
